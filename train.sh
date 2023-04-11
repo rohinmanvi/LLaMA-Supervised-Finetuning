bf6939af8cb178f433ac3f4b7ddffe93361bf1df
rohinmanvi <rohinmanvi@gmail.com>
2023-04-10 22:37:50 -0700

    update

--- train.sh
+++ train.sh
@@ -13,14 +13,14 @@ ml cuda/12.0.0
 ml gcc/12.1.0
 
 python3 train.py \
-    --lora_r 8 \
-    --lora_alpha 16 \
+    --lora_r 16 \
+    --lora_alpha 32 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path ./test.jsonl \
     --output_dir ./llama-driver \
     --num_train_epochs 10 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --learning_rate 3e-4 \
     --lr_scheduler_type "cosine" \
