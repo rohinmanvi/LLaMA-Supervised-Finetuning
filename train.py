2585f9ac49b55afb8202034e613543bbe7021cb3
rohinmanvi <rohinmanvi@gmail.com>
2023-04-11 00:47:10 -0700

    update

--- train.py
+++ train.py
@@ -70,15 +70,6 @@ class LoraArguments:
     lora_weight_path: str = ""
 
 
-def safe_save_model_for_hf_trainer(trainer: transformers.Trainer, output_dir: str):
-    """Collects the state dict and dump to disk."""
-    state_dict = trainer.model.state_dict()
-    if trainer.args.should_save:
-        cpu_state_dict = {key: value.cpu() for key, value in state_dict.items()}
-        del state_dict
-        trainer._save(output_dir, state_dict=cpu_state_dict)  # noqa
-
-
 def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer) -> Dict:
     """Tokenize a list of strings."""
     tokenized_list = [
