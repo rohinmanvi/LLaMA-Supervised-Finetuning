e3af8a2e8779fdfd804c9b25d6e5cbf882c5d9a2
rohinmanvi <rohinmanvi@gmail.com>
2023-04-14 00:26:32 -0700

    update

--- src/waypoint_train.py
+++ src/waypoint_train.py
@@ -11,17 +11,13 @@ from waypoint_driver_env import DriverEnv
 wandb.init(project="PPO_Waypoint")
 
 models_dir = "PPO_Waypoint"
-log_dir = "logs"
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
 env = DriverEnv()
 
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_dir)
+model = PPO("MlpPolicy", env, verbose=1)
 
 eval_callback = EvalCallback(env, best_model_save_path=models_dir, eval_freq=10000,
                              deterministic=True, render=False)
@@ -30,6 +26,6 @@ wandb_callback = WandbCallback()
 
 print("Training ...")
 
-model.learn(total_timesteps=10000000, tb_log_name=models_dir, callback=[eval_callback, wandb_callback])
+model.learn(total_timesteps=10000000, callback=[eval_callback, wandb_callback])
 
 print("Done Training")
