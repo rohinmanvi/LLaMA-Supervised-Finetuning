03a7a8edabc65989290f3e53205e1262707a0cec
rohinmanvi <rohinmanvi@gmail.com>
2023-04-16 03:26:00 -0700

    update

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ python3 src/train.py \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path data/waypoint_data.jsonl \
     --output_dir models/llama-waypoint-driver3 \
-    --num_train_epochs 15 \
+    --num_train_epochs 20 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-4 \
