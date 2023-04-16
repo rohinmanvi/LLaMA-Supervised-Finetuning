7c2a2ca056f90b97b88c3aa0210587f72fc61514
rohinmanvi <rohinmanvi@gmail.com>
2023-04-16 12:34:27 -0700

    update

--- train.sh
+++ train.sh
@@ -8,16 +8,16 @@ ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
 
 python3 src/train.py \
-    --lora_r 16 \
-    --lora_alpha 32 \
+    --lora_r 8 \
+    --lora_alpha 16 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path data/waypoint_data.jsonl \
-    --output_dir models/llama-waypoint-driver3 \
+    --output_dir models/llama-waypoint-driver4 \
     --num_train_epochs 20 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
-    --learning_rate 3e-4 \
+    --learning_rate 2e-5 \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
     --fp16 True \
