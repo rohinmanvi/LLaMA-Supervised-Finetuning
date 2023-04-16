f6ebc600d9d2aebb5a2dd907220685dd22c7be2b
rohinmanvi <rohinmanvi@gmail.com>
2023-04-16 03:33:17 -0700

    update

--- train.sh
+++ train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH -G 1
-#SBATCH -C GPU_MEM:32GB
+#SBATCH -C GPU_MEM:24GB
 #SBATCH --time=48:00:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
 
 python3 src/train.py \
-    --lora_r 8 \
-    --lora_alpha 16 \
+    --lora_r 16 \
+    --lora_alpha 32 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path data/waypoint_data.jsonl \
@@ -17,7 +17,7 @@ python3 src/train.py \
     --num_train_epochs 20 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
-    --learning_rate 1e-4 \
+    --learning_rate 3e-4 \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
     --fp16 True \
