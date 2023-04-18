c4a3fb911a708d7259e1f3287e236d776c3b1c4f
rohinmanvi <rohinmanvi@gmail.com>
2023-04-18 06:56:35 -0700

    update

--- src/evaluation.py
+++ src/evaluation.py
@@ -28,7 +28,7 @@ for example in examples:
     completion = example['completion']
 
     response = model_handler.generate_text(
-        peft_model='models/llama-waypoint-driver2',
+        peft_model='models/llama-waypoint-driver3',
         text=prompt,
         generation_config=generation_config
     )
