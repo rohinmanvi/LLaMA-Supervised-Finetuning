79415be91fd7b3b9a10a8ed2d9c82d719d3b6d50
rohinmanvi <rohinmanvi@gmail.com>
2023-04-18 20:52:07 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu
 #SBATCH -G 1
 #SBATCH -C GPU_MEM:32GB
-#SBATCH --time=24:00:00
+#SBATCH --time=36:00:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
@@ -14,7 +14,7 @@ python3 src/finetune.py \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path data/waypoint_sequence_data_large.jsonl \
     --output_dir models/finetune-llama-waypoint-driver \
-    --num_train_epochs 1 \
+    --num_train_epochs 2 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --learning_rate 3e-3 \
