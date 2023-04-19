0c40ae9c6c8f8ee5579a375e8e887135e6f3b4b8
rohinmanvi <rohinmanvi@gmail.com>
2023-04-19 16:55:02 -0700

    update

--- src/inference.py
+++ src/inference.py
@@ -36,6 +36,20 @@ print("=========================================================================
 print(prompt + "|" + response)
 print("============================================================================")
 
+prompt = "Observation: \"Our vehicle is going 7.0 m/s with a steering angle of 0.0\u00b0. The other vehicle is 44.4 m away and is 12.9\u00b0 to the right. It is going 7.0 m/s with a direction of 14.6\u00b0 to the right.\"\nAction: "
+
+response = model_handler.generate_text(
+    peft_model='models/finetune-llama-waypoint-driver',
+    text=prompt,
+    generation_config=generation_config
+)
+
+response = response[len(prompt):]
+
+print("============================================================================")
+print(prompt + "|" + response)
+print("============================================================================")
+
 prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 42.5 m away and is 39.0° to the left. It is going 7.0 m/s with a direction of 35.8° to the left."
 Action: """
 
@@ -51,8 +65,7 @@ print("=========================================================================
 print(prompt + "|" + response)
 print("============================================================================")
 
-prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 44.4 m away and is 12.9° to the left. It is going 7.0 m/s with a direction of 14.6° to the left."
-Action: """
+prompt = "Observation: \"Our vehicle is going 7.0 m/s with a steering angle of 0.0\u00b0. The other vehicle is 42.5 m away and is 35.6\u00b0 to the left. It is going 7.0 m/s with a direction of 38.5\u00b0 to the left.\"\nAction: "
 
 response = model_handler.generate_text(
     peft_model='models/finetune-llama-waypoint-driver',
