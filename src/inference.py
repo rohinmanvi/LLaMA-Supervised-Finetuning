fa3b1d51cb24e9b4dff106b981d4ab83f4a5cdd8
rohinmanvi <rohinmanvi@gmail.com>
2023-04-19 16:04:36 -0700

    update

--- src/inference.py
+++ src/inference.py
@@ -8,8 +8,11 @@ from model_handler import ModelHandler
 # prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 44.4 m away and is 12.9° to the right. It is going 7.0 m/s with a direction of 14.6° to the right."
 # Action: """
 
-prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 42.5 m away and is 39.0° to the left. It is going 7.0 m/s with a direction of 35.8° to the left."
+prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 42.5 m away and is 39.0° to the right. It is going 7.0 m/s with a direction of 35.8° to the right."
 Action: """
+
+# prompt = """Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 42.5 m away and is 39.0° to the left. It is going 7.0 m/s with a direction of 35.8° to the left."
+# Action: """
 
 # Observation: "Our vehicle is going 7.0 m/s with a steering angle of 0.0°. The other vehicle is 42.5 m away and is 39.0° to the left. It is going 7.0 m/s with a direction of 35.8° to the left."
 # Action:  (9.5 m, -16.9°)
