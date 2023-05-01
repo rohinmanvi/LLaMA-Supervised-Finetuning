3f2e9c5f8bc2cf697ec40d89e30654b162a0287c
rohinmanvi <rohinmanvi@gmail.com>
2023-04-30 20:22:32 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu
 #SBATCH -G 1
 #SBATCH -C GPU_MEM:32GB
-#SBATCH --time=48:00:00
+#SBATCH --time=36:00:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
@@ -12,8 +12,8 @@ python3 src/finetune.py \
     --lora_alpha 16 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
-    --data_path data/test_shortest_2.jsonl \
-    --output_dir models/sequence-driver-shortest-2 \
+    --data_path data/test_shorter_2.jsonl \
+    --output_dir models/sequence-driver-shorter-2 \
     --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
