c8e3604059996f8a8f2f65972744ff9254085042
rohinmanvi <rohinmanvi@gmail.com>
2023-04-30 20:53:53 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH -G 1
-#SBATCH -C GPU_MEM:32GB
-#SBATCH --time=36:00:00
+#SBATCH -C GPU_MEM:24GB
+#SBATCH --time=48:00:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
