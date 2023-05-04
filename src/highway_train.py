8ed49a04a38cb37b5a61c64c0582f1672458c195
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 15:57:05 -0700

    update

--- src/highway_train.py
+++ src/highway_train.py
@@ -2,6 +2,9 @@ import os
 import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 
 import wandb
 from wandb.integration.sb3 import WandbCallback
@@ -27,7 +30,7 @@ models_dir = "models/PPO_Highway"
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-env = gym.make("highway-v0")
+env = make_vec_env("highway-v0")
 
 model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
 
