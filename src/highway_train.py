ae5b9785d2c10ffb96982e72a026ed1c4826339c
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 15:58:38 -0700

    update

--- src/highway_train.py
+++ src/highway_train.py
@@ -30,9 +30,20 @@ models_dir = "models/PPO_Highway"
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
-env = make_vec_env("highway-v0")
-
-model = PPO(config["policy_type"], env, verbose=1, tensorboard_log=f"runs/{run.id}")
+n_cpu = 6
+batch_size = 64
+env = make_vec_env("highway-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
+env = Monitor(env)
+model = PPO(config["policy_type"],
+            env,
+            policy_kwargs=dict(net_arch=[dict(pi=[256, 256], vf=[256, 256])]),
+            n_steps=batch_size * 12 // n_cpu,
+            batch_size=batch_size,
+            n_epochs=10,
+            learning_rate=5e-4,
+            gamma=0.8,
+            verbose=2,
+            tensorboard_log=f"runs/{run.id}")
 
 eval_callback = EvalCallback(env, best_model_save_path=models_dir, eval_freq=10000, n_eval_episodes=100, deterministic=True, render=False)
 
