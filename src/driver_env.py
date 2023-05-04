971cc117941f33615970704abed459e305de48a4
rohinmanvi <rohinmanvi@gmail.com>
2023-05-03 17:56:29 -0700

    update

--- src/driver_env.py
+++ src/driver_env.py
@@ -105,8 +105,8 @@ class DriverEnv(gym.Env):
 
         return np.array(observation), float(reward), done, dict()
 
-    def reset(self):
-        self.state, observation = self._reset()
+    def reset(self, seed=None):
+        self.state, observation = self._reset(seed)
 
         self.steps = 0
 
@@ -219,14 +219,16 @@ class DriverEnv(gym.Env):
 
         return [ego_prime.v, ego_prime.phi, angle_to_agent, distance, agent_theta, agent_prime.v]
 
-    def _reset(self):
-        ego_theta = self.np_random.uniform(-0.75, 0.75)
+    def _reset(self, seed=None):
+        np_random, seed = gym.utils.seeding.np_random(seed)
+
+        ego_theta = np_random.uniform(-0.75, 0.75)
         ego_v = 7.0
 
-        agent_x = self.np_random.uniform(10.0, 50.0)
-        agent_theta = self.np_random.uniform(-0.75, 0.75)
+        agent_x = np_random.uniform(10.0, 50.0)
+        agent_theta = np_random.uniform(-0.75, 0.75)
         agent_v = 7.0
-        agent_phi = self.np_random.uniform(-0.1, 0.1)
+        agent_phi = np_random.uniform(-0.1, 0.1)
 
         ego = Vehicle(theta=ego_theta, v=ego_v)
         agent = Vehicle(x=agent_x, theta=agent_theta, v=agent_v, phi=agent_phi)
@@ -242,13 +244,7 @@ class DriverEnv(gym.Env):
         self.ego_positions.clear()
         self.agent_positions.clear()
 
-        self.seed()
-
         return s, o
-
-    def seed(self, seed=None):
-        self.np_random, seed = gym.utils.seeding.np_random(seed)
-        return [seed]
 
     def _map_action_index_to_obj(self, action_index):
         return Action(acceleration=action_index[0], steering_rate=action_index[1])
