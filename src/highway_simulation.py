9e0e20871609d6c96e51aeabdb43030310dbb4b9
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 20:14:41 -0700

    update

---
+++ src/highway_simulation.py
@@ -0,0 +1,23 @@
+import gymnasium as gym
+import torch as th
+from stable_baselines3 import PPO
+from torch.distributions import Categorical
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn import functional as F
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.vec_env import SubprocVecEnv
+import highway_env
+
+model = PPO.load("highway_ppo/model")
+env = gym.make("highway-fast-v0")
+for _ in range(1):
+    obs, info = env.reset()
+    done = truncated = False
+    while not (done or truncated):
+        action, _ = model.predict(obs)
+        print(action)
+        obs, reward, done, truncated, info = env.step(action)
+        print(obs)
