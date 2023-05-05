bae30583fabba79baf18eef3a04e90eaf85a6513
rohinmanvi <rohinmanvi@gmail.com>
2023-05-05 16:03:07 -0700

    update

--- src/create_highway_sequence_training_data.py
+++ src/create_highway_sequence_training_data.py
@@ -10,36 +10,34 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import SubprocVecEnv
 import highway_env
+import json
 
 np.set_printoptions(suppress=True)
 
 model = PPO.load("highway_ppo/model")
 env = gym.make("highway-fast-v0")
 
-data = []
+# Open the file in append mode
+with open("data/highway_sequence_data_incremental.jsonl", "a") as f:
+    for i in range(10000):
+        print(i)
 
-for i in range(10000):
-    print(i)
+        obs, info = env.reset()
+        done = truncated = False
 
-    obs, info = env.reset()
-    done = truncated = False
+        sequence = ""
 
-    sequence = ""
+        while not (done or truncated):
+            observation_string = f"Observation:\n{str(np.round(obs, 3))}"
+            sequence += observation_string + "\n"
 
-    while not (done or truncated):
-        observation_string = f"Observation:\n{str(np.round(obs, 3))}"
-        sequence += observation_string + "\n"
+            action, _ = model.predict(obs)
 
-        action, _ = model.predict(obs)
-        
-        action_string = f"Action: {str(action)}"
-        sequence += action_string + "\n"
+            action_string = f"Action: {str(action)}"
+            sequence += action_string + "\n"
 
-        obs, reward, done, truncated, info = env.step(action)
+            obs, reward, done, truncated, info = env.step(action)
 
-    data.append({"text": sequence})
-
-with open("data/highway_sequence_data.jsonl", "w") as f:
-    for datum in data:
-        json.dump(datum, f)
+        # Write the data to the file
+        json.dump({"text": sequence}, f)
         f.write("\n")
