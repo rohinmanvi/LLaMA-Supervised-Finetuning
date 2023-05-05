b1cb0781d99779089e1583ad84fb427bbe0edb90
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 17:54:09 -0700

    update

--- src/highway_train.py
+++ src/highway_train.py
@@ -17,7 +17,7 @@ from wandb.integration.sb3 import WandbCallback
 
 config = {
     "policy_type": "MlpPolicy",
-    "total_timesteps": 50000,
+    "total_timesteps": 1000000,
     "env_name": "highway-fast-v0",
 }
 
