9c3c0b1f7526e7136bdd04524ed8ecebe7e7a1e1
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 17:00:09 -0700

    update

--- src/highway_train.py
+++ src/highway_train.py
@@ -1,3 +1,4 @@
+import os
 import gymnasium as gym
 import torch as th
 from stable_baselines3 import PPO
@@ -9,40 +10,36 @@ from torch.nn import functional as F
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.callbacks import EvalCallback
 import highway_env
 
+import wandb
+from wandb.integration.sb3 import WandbCallback
 
-# ==================================
-#        Main script
-# ==================================
+n_cpu = 6
+batch_size = 64
+env = make_vec_env("highway-fast-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
+model = PPO("MlpPolicy",
+            env,
+            policy_kwargs=dict(net_arch=[dict(pi=[256, 256], vf=[256, 256])]),
+            n_steps=batch_size * 12 // n_cpu,
+            batch_size=batch_size,
+            n_epochs=10,
+            learning_rate=5e-4,
+            gamma=0.8,
+            verbose=2,
+            tensorboard_log="highway_ppo/")
 
-if __name__ == "__main__":
-    train = True
-    if train:
-        n_cpu = 6
-        batch_size = 64
-        env = make_vec_env("highway-fast-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
-        model = PPO("MlpPolicy",
-                    env,
-                    policy_kwargs=dict(net_arch=[dict(pi=[256, 256], vf=[256, 256])]),
-                    n_steps=batch_size * 12 // n_cpu,
-                    batch_size=batch_size,
-                    n_epochs=10,
-                    learning_rate=5e-4,
-                    gamma=0.8,
-                    verbose=2,
-                    tensorboard_log="highway_ppo/")
-        # Train the agent
-        model.learn(total_timesteps=int(2e4))
-        # Save the agent
-        model.save("highway_ppo/model")
+config = {"total_timesteps": int(2e4)}
+run = wandb.init(project="highway_ppo", config=config, sync_tensorboard=True)
+models_dir = "models/highway_ppo"
 
-    model = PPO.load("highway_ppo/model")
-    env = gym.make("highway-fast-v0")
-    for _ in range(5):
-        obs, info = env.reset()
-        done = truncated = False
-        while not (done or truncated):
-            action, _ = model.predict(obs)
-            obs, reward, done, truncated, info = env.step(action)
-            env.render()
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+eval_callback = EvalCallback(env, best_model_save_path=models_dir, eval_freq=4000, n_eval_episodes=100, deterministic=True, render=False)
+wandb_callback = WandbCallback(verbose=2)
+
+model.learn(total_timesteps=config["total_timesteps"], callback=[eval_callback, wandb_callback])
+
+run.finish()
