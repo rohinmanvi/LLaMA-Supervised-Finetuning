96b68e2a30a14fe8aa39cc56f7751735053c324f
rohinmanvi <rohinmanvi@gmail.com>
2023-05-04 17:03:49 -0700

    update

--- src/highway_train.py
+++ src/highway_train.py
@@ -13,9 +13,6 @@ from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.callbacks import EvalCallback
 import highway_env
 
-import wandb
-from wandb.integration.sb3 import WandbCallback
-
 n_cpu = 6
 batch_size = 64
 env = make_vec_env("highway-fast-v0", n_envs=n_cpu, vec_env_cls=SubprocVecEnv)
@@ -30,16 +27,11 @@ model = PPO("MlpPolicy",
             verbose=2,
             tensorboard_log="highway_ppo/")
 
-config = {"total_timesteps": int(2e4)}
-run = wandb.init(project="highway_ppo", config=config, sync_tensorboard=True)
 models_dir = "models/highway_ppo"
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
 eval_callback = EvalCallback(env, best_model_save_path=models_dir, eval_freq=4000, n_eval_episodes=100, deterministic=True, render=False)
-wandb_callback = WandbCallback(verbose=2)
 
-model.learn(total_timesteps=config["total_timesteps"], callback=[eval_callback, wandb_callback])
-
-run.finish()
+model.learn(total_timesteps=int(2e4), callback=[eval_callback])
