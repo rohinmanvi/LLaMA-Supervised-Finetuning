62bdf5a3ed047b0169dc333e5e182b3868e8fca9
rohinmanvi <rohinmanvi@gmail.com>
2023-05-05 17:56:31 -0700

    update

---
+++ src/highway_simulation.py
@@ -0,0 +1,37 @@
+import gymnasium as gym
+import torch as th
+from stable_baselines3 import PPO
+from torch.distributions import Categorical
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn import functional as F
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from stable_baselines3.common.vec_env import SubprocVecEnv
+import highway_env
+from gymnasium.wrappers import RecordVideo
+
+np.set_printoptions(suppress=True)
+
+model = PPO.load("models/roundabout_ppo/best_model")
+env = gym.make("roundabout-v0", render_mode='rgb_array')
+
+def record_videos(env, video_folder="videos"):
+    wrapped = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda e: True)
+
+    # Capture intermediate frames
+    env.unwrapped.set_record_video_wrapper(wrapped)
+
+    return wrapped
+
+env = record_videos(env)
+
+for _ in range(1):
+    obs, info = env.reset()
+    done = truncated = False
+    while not (done or truncated):
+        action, _ = model.predict(obs)
+        obs, reward, done, truncated, info = env.step(action)
+
+env.close()
