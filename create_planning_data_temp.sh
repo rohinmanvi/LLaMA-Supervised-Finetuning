6b7b1f9447fbe08bfb2b8b5d7a8519d52ece08ad
rohinmanvi <rohinmanvi@gmail.com>
2023-05-06 02:33:17 -0700

    update

---
+++ create_planning_data_temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -G 1
+#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+
+ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
+
+python3 src/create_planning_data_temp.py
---
+++ src/create_planning_data_temp.py
@@ -0,0 +1,45 @@
+import numpy as np
+import gymnasium as gym
+import highway_env
+from gymnasium.wrappers import RecordVideo
+from rl_agents.agents.common.factory import agent_factory
+import json
+
+np.set_printoptions(suppress=True)
+
+env = gym.make("intersection-v0", render_mode='rgb_array')
+
+agent_config = {
+    "__class__": "<class 'rl_agents.agents.tree_search.deterministic.DeterministicPlannerAgent'>",
+    "env_preprocessors": [{"method":"simplify"}],
+    "budget": 50,
+    "gamma": 0.7,
+}
+agent = agent_factory(env, agent_config)
+
+with open("data/intersection_planner_sequence_data_incremental.jsonl", "a") as f:
+    for i in range(10000):
+        print(i)
+
+        obs, info = env.reset()
+        done = truncated = False
+
+        sequence = ""
+
+        while not (done or truncated):
+            observation_string = f"Observation:\n{str(np.round(obs, 3))}"
+            sequence += observation_string + "\n"
+
+            action = agent.act(obs)
+
+            action_string = f"Action: {str(action)}"
+            sequence += action_string + "\n"
+
+            obs, reward, done, truncated, info = env.step(action)
+
+        # Write the data to the file
+        json.dump({"text": sequence}, f)
+        f.write("\n")
+        f.flush()
+
+env.close()
