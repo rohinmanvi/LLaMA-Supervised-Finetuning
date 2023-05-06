b82d4b15b6b9168eb554d5d7b69a10a0a4dc7ecb
rohinmanvi <rohinmanvi@gmail.com>
2023-05-06 16:14:50 -0700

    update

---
+++ src/combine_planner_data.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+input_directory = "data/"
+output_file = "data/highway_planner_sequence_data_incremental_total.jsonl"
+
+# Check if the output file exists and remove it
+if os.path.exists(output_file):
+    os.remove(output_file)
+
+# Iterate through the JSONL files and combine them
+i = 0
+while True:
+    input_file = f"{input_directory}highway_planner_sequence_data_incremental_{i}.jsonl"
+    if not os.path.exists(input_file):
+        break
+
+    with open(input_file, "r") as infile, open(output_file, "a") as outfile:
+        for line in infile:
+            data = json.loads(line)
+            outfile.write(json.dumps(data) + "\n")
+    i += 1
+
+print("All JSONL files have been combined into", output_file)
