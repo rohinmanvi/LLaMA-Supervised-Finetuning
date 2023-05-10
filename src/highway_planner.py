f1ebbd41f59bee5252b34a94bd1cf82ed85d7049
rohinmanvi <rohinmanvi@gmail.com>
2023-05-09 23:18:31 -0700

    update

--- src/highway_planner.py
+++ src/highway_planner.py
@@ -6,7 +6,17 @@ from rl_agents.agents.common.factory import agent_factory
 
 np.set_printoptions(suppress=True)
 
-env = gym.make("highway-fast-v0", render_mode='rgb_array')
+env = gym.make("roundabout-v0", render_mode='rgb_array')
+
+# Add this class to define the reward clamping wrapper
+class RewardClampingWrapper(gym.Wrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        clamped_reward = np.clip(reward, 0, 1)
+        return obs, clamped_reward, done, info
 
 agent_config = {
     "__class__": "<class 'rl_agents.agents.tree_search.deterministic.DeterministicPlannerAgent'>",
@@ -16,7 +26,7 @@ agent_config = {
 }
 agent = agent_factory(env, agent_config)
 
-def record_videos(env, video_folder="videos_expert"):
+def record_videos(env, video_folder="videos_roundabout_expert"):
     wrapped = RecordVideo(env, video_folder=video_folder, episode_trigger=lambda e: True)
 
     # Capture intermediate frames
@@ -30,9 +40,9 @@ for _ in range(5):
     obs, info = env.reset()
     done = truncated = False
     while not (done or truncated):
-        print(f"Observation:\n{str(np.round(obs, 3))}")
+        print(f"Observation:\n{np.round(obs, 3)}")
         action = agent.act(obs)
-        print(f"Action: {str(action)}")
+        print(f"Action: {action}")
         obs, reward, done, truncated, info = env.step(action)
 
 env.close()
