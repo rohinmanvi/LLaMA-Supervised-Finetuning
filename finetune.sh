a86ce9a9957d8ad49b5bf850a837b0df11a06e50
rohinmanvi <rohinmanvi@gmail.com>
2023-05-13 16:21:47 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -12,9 +12,9 @@ python3 src/finetune.py \
     --lora_alpha 16 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
-    --data_path data/roundabout_planner_sequence_data_incremental_final_2.jsonl \
-    --output_dir models/roundabout-driver-final-3 \
-    --num_train_epochs 2 \
+    --data_path data/highway_planner_sequence_data_incremental_final_2.jsonl \
+    --output_dir models/highway-driver-final-4 \
+    --num_train_epochs 5 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-3 \
--- src/create_graphs.py
+++ src/create_graphs.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+pastelBlue = "#0072B2"
+pastelRed = "#F5615C"
+
 # Load data
 llama_df = pd.read_csv('llama_highway_data.csv')
 planner_df = pd.read_csv('planner_highway_data.csv')
@@ -9,8 +12,8 @@ planner_df = pd.read_csv('planner_highway_data.csv')
 fig, ax = plt.subplots(tight_layout=True)
 
 # Plot histograms
-ax.hist(llama_df['total_rewards'], bins=20, alpha=0.5, color='blue', label='LLaMA')
-ax.hist(planner_df['total_rewards'], bins=20, alpha=0.5, color='red', label='OPD')
+ax.hist(llama_df['total_rewards'], bins=20, alpha=0.5, color=pastelBlue, label='LLaMA')
+ax.hist(planner_df['total_rewards'], bins=20, alpha=0.5, color=pastelRed, label='OPD')
 
 # Set labels
 ax.set_xlabel('Total Rewards')
