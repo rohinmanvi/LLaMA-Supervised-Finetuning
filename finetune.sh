1e6fb5ce979067fd490939e91e34fe06467b6449
rohinmanvi <rohinmanvi@gmail.com>
2023-05-13 18:58:52 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu
 #SBATCH -G 1
 #SBATCH -C GPU_MEM:32GB
-#SBATCH --time=00:30:00
+#SBATCH --time=48:00:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
@@ -13,13 +13,13 @@ python3 src/finetune.py \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
     --data_path data/highway_planner_sequence_data_incremental_final_2.jsonl \
-    --output_dir models/highway-driver-final-4 \
-    --num_train_epochs 5 \
+    --output_dir models/highway-driver-final-5 \
+    --num_train_epochs 2 \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-3 \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
     --fp16 True \
-    --model_max_length 2 \
+    --model_max_length 4096 \
     --report_to wandb
