6646b719db5d9c258c9764bf9b47e8242708149d
rohinmanvi <rohinmanvi@gmail.com>
2023-05-13 17:13:23 -0700

    update

--- src/create_graphs.py
+++ src/create_graphs.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 pastelBlue = "#0072B2"
 pastelRed = "#F5615C"
+pastelGreen = "#009E73"
+pastelPurple = "#8770FE"
 
 # Load data
 llama_df = pd.read_csv('llama_highway_data.csv')
@@ -23,4 +25,40 @@ ax.set_ylabel('Frequency')
 ax.legend()
 
 # Save the figure
-plt.savefig('histogram.pdf')
+plt.savefig('plots/rewards_histogram.pdf')
+
+# Create a figure for min-max comparison
+fig1, ax1 = plt.subplots(tight_layout=True)
+
+# Plot histograms
+ax1.hist(llama_df['min_inference_times'], bins=20, alpha=0.5, color=pastelGreen, label='First Inference Times')
+ax1.hist(llama_df['max_inference_times'], bins=20, alpha=0.5, color=pastelPurple, label='Last Inference Times')
+
+# Set labels
+ax1.set_xlabel('Inference Times')
+ax1.set_ylabel('Frequency')
+
+# Show legend
+ax1.legend()
+
+# Save the figure
+plt.savefig('plots/min_max_inference_times_histogram.pdf')
+
+# Create a figure for avg comparison
+fig2, ax2 = plt.subplots(tight_layout=True)
+
+# Plot histograms
+ax2.hist(llama_df['avg_inference_times'], bins=20, alpha=0.5, color=pastelRed, label='LLaMA Average Inference Times')
+ax2.hist(planner_df['avg_inference_times'], bins=20, alpha=0.5, color=pastelBlue, label='OPD Average Planning Times')
+
+# Set labels
+ax2.set_xlabel('Average Inference Times')
+ax2.set_ylabel('Frequency')
+
+# Show legend
+ax2.legend()
+
+# Save the figure
+plt.savefig('plots/avg_inference_times_histogram.pdf')
+
+
