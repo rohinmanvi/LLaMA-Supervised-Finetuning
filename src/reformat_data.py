f4c8286afbec1b8324eba3ae2f78c669354184a3
rohinmanvi <rohinmanvi@gmail.com>
2023-05-14 16:26:53 -0700

    update

---
+++ src/reformat_data.py
@@ -0,0 +1,13 @@
+import json
+
+with open('roundabout_planner_sequence_data_incremental_final_2.jsonl', 'r') as f_in:
+    with open('roundabout_planner_data_incremental.jsonl', 'w') as f_out:
+        for line in f_in:
+            data = json.loads(line)
+            text = data['text']
+            
+            # split into observation-action pairs
+            pairs = text.split('Observation:')
+            for pair in pairs[1:]:  # ignore the first split which is empty
+                new_data = {"text": "Observation:" + pair}
+                f_out.write(json.dumps(new_data) + '\n')
