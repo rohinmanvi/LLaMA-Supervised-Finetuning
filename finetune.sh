6dc9774495170ec9798f8a749e4b72bdcaa9a68b
rohinmanvi <rohinmanvi@gmail.com>
2023-05-14 23:52:14 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -2,7 +2,7 @@
 #SBATCH -p gpu
 #SBATCH -G 1
 #SBATCH -C GPU_MEM:32GB
-#SBATCH --time=48:00:00
+#SBATCH --time=00:30:00
 
 ml python/3.9.0 cuda/12.0.0 gcc/12.1.0
 nvidia-smi
@@ -19,7 +19,7 @@ python3 src/finetune.py \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-3 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 100 \
+    --logging_steps 10 \
     --fp16 True \
-    --model_max_length 256 \
+    --model_max_length 4096 \
     --report_to wandb
