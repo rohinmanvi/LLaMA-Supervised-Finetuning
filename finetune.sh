dc548f84c3e6f1635f969aa8b1d3bd3560a01c91
rohinmanvi <rohinmanvi@gmail.com>
2023-05-14 18:48:47 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -15,11 +15,11 @@ python3 src/finetune.py \
     --data_path data/roundabout_planner_data_incremental.jsonl \
     --output_dir models/roundabout-no-sequence-test \
     --num_train_epochs 2 \
-    --per_device_train_batch_size 32 \
+    --per_device_train_batch_size 128 \
     --gradient_accumulation_steps 8 \
     --learning_rate 1e-3 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 10 \
+    --logging_steps 1 \
     --fp16 True \
     --model_max_length 256 \
     --report_to wandb
