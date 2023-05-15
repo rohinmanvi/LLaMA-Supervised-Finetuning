56acfc305e278714f8324008c3cf13d730add47f
rohinmanvi <rohinmanvi@gmail.com>
2023-05-15 00:12:43 -0700

    update

--- src/finetune.py
+++ src/finetune.py
@@ -92,18 +92,17 @@ def train():
 
     tokenizer = transformers.LlamaTokenizer.from_pretrained(model_args.model_name_or_path, add_eos_token=True)
     tokenizer.pad_token_id = 0
-
+    
     text = []
 
     with open(data_args.data_path, 'r') as f:
         for line in f:
             data = json.loads(line)
-            text.append(data['text'])  # append just the text, not the entire data dict
+            text.append(data)
 
     print("Determining max sequence length...")
 
-    # you might want to consider handling this in chunks if your dataset is large
-    tokenized_text = [tokenizer.encode(t, truncation=False) for t in text]
+    tokenized_text = [tokenizer.encode(t['text'], truncation=False) for t in text]
     max_length = max(len(t) for t in tokenized_text)
 
     if max_length < training_args.model_max_length:
@@ -112,8 +111,8 @@ def train():
     else:
         print(f"Keeping model_max_length at {training_args.model_max_length}")
 
-    data = datasets.Dataset.from_dict({'text': text})
-    data = data.shuffle().map(lambda t: tokenizer(t, padding='max_length', max_length=training_args.model_max_length, truncation=True))
+    data = datasets.Dataset.from_list(text)
+    data = data.shuffle().map(lambda t: tokenizer(t['text'], padding='max_length', max_length=training_args.model_max_length, truncation=True))
 
     trainer = transformers.Trainer(
         model=model,
