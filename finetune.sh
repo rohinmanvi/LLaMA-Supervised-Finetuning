26439e1bc173c7f877178d93cc235c22b0602e70
rohinmanvi <rohinmanvi@gmail.com>
2023-05-15 01:05:06 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -21,5 +21,5 @@ python3 src/finetune.py \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
     --fp16 True \
-    --model_max_length 4096 \
+    --model_max_length 163 \
     --report_to wandb
