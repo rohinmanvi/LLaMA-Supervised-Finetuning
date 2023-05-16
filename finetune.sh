a3292dd007b2bc5aa8077560176a0bb89c7ba3ca
rohinmanvi <rohinmanvi@gmail.com>
2023-05-15 20:37:09 -0700

    update

--- finetune.sh
+++ finetune.sh
@@ -12,9 +12,9 @@ python3 src/finetune.py \
     --lora_alpha 16 \
     --lora_dropout 0.05 \
     --model_name_or_path decapoda-research/llama-7b-hf \
-    --data_path data/roundabout_planner_data_incremental.jsonl \
-    --output_dir models/roundabout-no-sequence-2 \
-    --num_train_epochs 9 \
+    --data_path data/highway_planner_data_incremental.jsonl \
+    --output_dir models/highway-no-sequence-2 \
+    --num_train_epochs 3 \
     --per_device_train_batch_size 128 \
     --gradient_accumulation_steps 1 \
     --learning_rate 1e-3 \
