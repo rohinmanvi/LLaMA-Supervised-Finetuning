d0c116fc247f6ea08625250b8fdb28d54d4a0fab
rohinmanvi <rohinmanvi@gmail.com>
2023-05-16 11:56:22 -0700

    update

--- src/highway_planner.py
+++ src/highway_planner.py
@@ -44,7 +44,7 @@ for episode in range(3):
     inference_times = []
 
     while not (done or truncated):
-        print(f"Observation:\n{np.round(obs, 5)}")
+        print(f"Observation:\n{obs}")
         
         start_time = time.time()
         action = agent.act(obs)
