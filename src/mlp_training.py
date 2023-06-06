60cb4e24d09b652a713947063879ce93e21c1685
rohinmanvi <rohinmanvi@gmail.com>
2023-06-05 18:50:43 -0700

    update

--- src/mlp_training.py
+++ src/mlp_training.py
@@ -11,7 +11,7 @@ class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(MLP, self).__init__()
         self.layer1 = nn.Linear(input_size, hidden_size)
-        # self.layer2 = nn.Linear(hidden_size, hidden_size)
+        self.layer2 = nn.Linear(hidden_size, hidden_size)
         self.layer3 = nn.Linear(hidden_size, num_classes)
         self.relu = nn.ReLU()
 
