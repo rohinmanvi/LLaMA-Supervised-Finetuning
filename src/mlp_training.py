72b6bd6f4655d7718e2ac0bd61d31b68a50d0a23
rohinmanvi <rohinmanvi@gmail.com>
2023-06-05 18:48:39 -0700

    update

--- src/mlp_training.py
+++ src/mlp_training.py
@@ -11,19 +11,16 @@ class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(MLP, self).__init__()
         self.layer1 = nn.Linear(input_size, hidden_size)
-        self.layer2 = nn.Linear(hidden_size, hidden_size)
-        self.layer3 = nn.Linear(hidden_size, hidden_size)
-        self.layer4 = nn.Linear(hidden_size, num_classes)
+        # self.layer2 = nn.Linear(hidden_size, hidden_size)
+        self.layer3 = nn.Linear(hidden_size, num_classes)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         out = self.layer1(x)
-        out = self.relu(out)
-        out = self.layer2(out)
+        # out = self.relu(out)
+        # out = self.layer2(out)
         out = self.relu(out)
         out = self.layer3(out)
-        out = self.relu(out)
-        out = self.layer4(out)
         return out
 
 def load_data(file):
